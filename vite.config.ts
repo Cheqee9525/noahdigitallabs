ae29e29959e71c5afa61b954fcc2c2a3b6740217
Noah <Chequemadrigal@gmail.com>
2026-03-05 21:57:03 -0800

    Fix sitemap import

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { inspectAttr } from 'kimi-plugin-inspect-react'
-import sitemap from 'vite-plugin-sitemap'
+import  sitemap   from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
